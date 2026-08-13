663b7ad94826159935352f32300d3b6f3e46f126
nihpat19 <nihpat77@gmail.com>
2026-08-13 14:12:21 -0500

    Add new VDI for Diamond dataset with correct alignment matrix

---
+++ neurd/vdi_microns_diamond.py
@@ -0,0 +1,64 @@
+import numpy as np
+from pathlib import Path
+from datasci_tools import numpy_utils as nu
+
+
+from . import vdi_default as vdi_def
+
+parameters_config_filename = "parameters_config_microns.py"
+config_filepath = str((
+    Path(__file__).parents[0]
+    / Path(f"parameter_configs/{parameters_config_filename}")
+).absolute())
+
+default_settings = dict(
+    source = "microns",
+    
+    parameters_config_filepaths = config_filepath,
+    
+    synapse_filepath = None,   
+)
+
+
+class DataInterfaceMicronsDiamond(vdi_def.DataInterfaceDefault):
+    
+    def __init__(
+        self,
+        **kwargs
+        ):
+        
+        for k,v in default_settings.items():
+            if k == "parameters_config_filepaths":
+                parameters_config_filepaths = kwargs.get(k,[])
+                parameters_config_filepaths = nu.to_list(parameters_config_filepaths)
+                parameters_config_filepaths = nu.to_list(v) + parameters_config_filepaths
+                kwargs["parameters_config_filepaths"] = parameters_config_filepaths
+            else:
+                if k not in kwargs:
+                    kwargs[k] = v
+                
+        
+        super().__init__(
+            **kwargs
+        )
+        
+    @property
+    def voxel_to_nm_scaling(self):
+        return np.array([4,4,40])
+    
+    def segment_id_to_synapse_df(
+        self,
+        *args,
+        **kwargs):
+        return super().segment_id_to_synapse_df(
+            *args,
+            **kwargs
+        )
+    
+    def get_align_matrix(self,*args,**kwargs):
+        return np.array([[1,0,0],[0,-1,0],[0,0,1]]) 
+    
+    
+    
+volume_data_interface = DataInterfaceMicronsDiamond()
+
